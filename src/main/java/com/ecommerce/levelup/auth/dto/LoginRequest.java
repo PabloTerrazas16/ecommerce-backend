7f5753fd57e9dd63ee60dc8642715cd77d2a8a66
PabloTerrazas16 <pa.terrazas@duocuc.cl>
2025-11-11 16:23:30 -0300

    Se me olvidó este DTO xd

--- src/main/java/com/ecommerce/levelup/auth/dto/LoginRequest.java
+++ src/main/java/com/ecommerce/levelup/auth/dto/LoginRequest.java
@@ -1,4 +1,18 @@
 package com.ecommerce.levelup.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
